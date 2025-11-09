7604e1c9426b884f41f40997a36b78528c8f56ae
Всеволод Гоголев <Tyuiu.Gogolevvm@outlook.com>
2025-11-09 17:44:33 +0300

    Task1V22

--- Tyuiu.GogolevVM.Sprint4.Task1.V22.Lib/DataService.cs
+++ Tyuiu.GogolevVM.Sprint4.Task1.V22.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.GogolevVM.Sprint4.Task1.V22.Lib
         public int Calculate(int[] array)
         {
             int p = 1;
-            for (int i = 2; i < array.Length - 2; i++)
+            for (int i = 0; i < array.Length ; i++)
             {
                 if (array[i] % 2 != 0)
                 {
